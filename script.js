6eb7fd413ba57a4b5ccc3ddb7ad20c08d6cc0ced
pjm91 <pramodram91@gmail.com>
2025-02-01 00:07:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.querySelectorAll('.code').forEach((input, index, inputs) => {
+  input.addEventListener('input', function() {
+    // Focus the next input field when a value is entered
+    if (this.value) {
+      const nextInput = inputs[index + 1];
+      if (nextInput) nextInput.focus();
+    }
+  });
+
+  input.addEventListener('keydown', function(e) {
+    // If backspace is pressed, focus the previous input field
+    if (e.key === 'Backspace' && !this.value) {
+      const prevInput = inputs[index - 1];
+      if (prevInput) prevInput.focus();
+    }
+  });
+});
